a6f3bad3ff55c3d5c2366420e5d28a355708057b
VictorPedr0za <pedrozaholguinvictormanuel@gmail.com>
2026-08-05 20:39:19 -0500

    Cambio

---
+++ rentacars/src/main/java/com/rentacars/RentacarsApplication.java
@@ -0,0 +1,13 @@
+package com.rentacars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentacarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentacarsApplication.class, args);
+	}
+
+}
---
+++ rentacars/src/test/java/com/rentacars/RentacarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentacars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentacarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
