dffb866a60b9f8933a5a7ed05ce3e34e974052a5
David Arango <117123190+daviiii-12@users.noreply.github.com>
2026-08-19 16:11:24 -0500

    udapted, tienda request y tienda repository

--- src/main/java/com/rentacars/dto/request/UpdateTiendaRequest.java
+++ src/main/java/com/rentacars/dto/request/UpdateTiendaRequest.java
@@ -1,4 +1,12 @@
 package com.rentacars.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UpdateTiendaRequest {
+    private String nombre;
+    private String ciudad;
+    private String direccion;
 }
